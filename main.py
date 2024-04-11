0a9db28a5c3ec5d2bd28ebb5aec8ab14e3c45da7
olive1rax <contato@olive1ra.xyz>
2024-04-10 21:56:03 -0300

    Add: Função receber mensagem.

--- main.py
+++ main.py
@@ -5,11 +5,29 @@ import time
 from datetime import date
 
 
-def receber_mensagem(connection: socket.socket):
+def receber_mensagem(connection: socket.socket): # Função para receber e tratar as mensagens.
     while True:
         try:
             msg = connection.recv(1024)
             if msg:
                 print(msg.decode())
             else:
-                connection.closes
+                connection.close()
+                break
+
+        except Exception as e:
+            print(f'Ocorreu um erro: {e}') 
+            connection.close()
+            break
+
+connections = [] # Variável para salvar as conexões dos clientes.
+
+
+def usuario(connection: socket.socket):
+    while True:
+        try:
+            msg = connection.recv(1024)
+            msg_dumped = json.loads(msg)
+            
+
+
