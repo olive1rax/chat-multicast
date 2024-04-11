fee19655fcf29992ed3d96babb50a63d3ad84480
olive1rax <contato@olive1ra.xyz>
2024-04-10 21:46:01 -0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import socket
+import threading
+import json
+import time
+from datetime import date
+
+
+def receber_mensagem(connection: socket.socket):
+    while True:
+        try:
+            msg = connection.recv(1024)
+            if msg:
+                print(msg.decode())
+            else:
+                connection.close
