fdb2e03fdbc1b377443464a0fb3d725c90750733
olive1rax <contato@olive1ra.xyz>
2024-04-30 20:06:31 -0300

    save

--- cliente.py
+++ cliente.py
@@ -40,30 +40,6 @@ def receber_mensagem(connection: socket.socket):
             fechar_conexao()
             break
 
-def selecionar_arquivo():
-    root = tk.Tk()
-    root.withdraw()  # Oculta a janela principal
-
-    caminho_arquivo = filedialog.askopenfilename()
-    root.destroy()  # Fecha a janela depois de selecionar o arquivo
-    return caminho_arquivo
-
-def enviar_arquivo():
-    global socket_instance
-    caminho_arquivo = selecionar_arquivo()
-
-    if caminho_arquivo:
-        with open(caminho_arquivo, "rb") as file:
-            arquivo_bytes = file.read()
-            payload = {
-                "comando": "/arquivo",
-                "nome_arquivo": os.path.basename(caminho_arquivo),
-                "conteudo_arquivo": arquivo_bytes
-            }
-            socket_instance.send(json.dumps(payload).encode())
-    else:
-        print("Nenhum arquivo selecionado.")
-
 def conectar_servidor(op_servidor, usuario):
     global socket_instance
     try:
@@ -103,47 +79,3 @@ def enviar_mensagem():
             }
             socket_instance.send(json.dumps(payload).encode())
             entrada_mensagem.delete(0, tk.END)
-
-def criar_interface_grafica():
-    global caixa_chat, entrada_mensagem
-    root = tk.Tk()
-    root.title("Chat")
-
-    # Caixa de chat
-    caixa_chat = tk.Text(root, height=20, width=50)
-    caixa_chat.config(state=tk.DISABLED)
-    caixa_chat.pack(padx=10, pady=10)
-
-    # Entrada de mensagem
-    entrada_mensagem = tk.Entry(root, width=50)
-    entrada_mensagem.pack(padx=10, pady=(0, 10))
-
-    # Botão de enviar
-    botao_enviar = tk.Button(root, text="Enviar", command=enviar_mensagem)
-    botao_enviar.pack(padx=10, pady=(0, 10))
-
-    root.mainloop()
-
-op = int(input("Ação desejada:\n"
-               "1 - Entrar em um servidor\n"
-               "0 - Fechar\n"))
-
-if op == 1:
-    usuario = str(input("Digite seu nome de usuário: "))
-
-    print("Servidores disponíveis:")
-    for i, servidor in enumerate(SERVIDORES):
-        print(f"{i+1} - {servidor['nome']}")
-
-    op_servidor = int(input("Escolha o servidor desejado: "))
-
-    if socket_instance and SERVIDORES[op_servidor - 1]['ip'] == socket_instance.getpeername()[0]:
-        print("Você já está conectado a este servidor!")
-    else:
-        conectar_servidor(op_servidor, usuario)
-
-    criar_interface_grafica()
-
-elif op == 0:
-    print("Fechando...")
-    exit()
